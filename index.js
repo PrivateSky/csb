426ef84ff08cfdbd09fb2b463084d611566ebc89
skutner <sava.dumitru.daniel@gmail.com>
2019-11-15 16:50:15 +0200

    implemented readFile and writeFile functions in RawCSB

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-module.exports.RootCSB = RootCSB;
 module.exports.RawCSB = require('./lib/RawCSB');
-module.exports.CSBIdentifier = require('./lib/CSBIdentifier');
-$$.loadLibrary("csb", require("./flows/index"));
 
 
