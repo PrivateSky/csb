3c4fab8bbe0c6397ed8448bb904aa92945bdefb6
skutner <sava.dumitru.daniel@gmail.com>
2019-12-03 18:11:43 +0200

    implemented class SeedCage

---
+++ tests/SeedCageTest.js
@@ -0,0 +1,34 @@
+require("../../../psknode/bundles/pskruntime");
+require("../../../psknode/bundles/psknode");
+require("../../../psknode/bundles/consoleTools");
+
+const SeedCage = require("../lib/SeedCage");
+const double_check = require("../../double-check");
+const assert = double_check.assert;
+
+double_check.createTestFolder("cageFolder", (err, testFolder) => {
+    if (err) {
+        throw err;
+    }
+
+    assert.callback("SeedCageTest", (callback) => {
+        const seedCage = new SeedCage(testFolder);
+        const compactSeed = "testSeed";
+        const pin = "12345";
+
+        seedCage.saveSeed(pin, compactSeed, (err) => {
+            if (err) {
+                throw err;
+            }
+
+            seedCage.loadSeed(pin, (err, loadedSeed) => {
+                if (err) {
+                    throw err;
+                }
+
+                assert.equal(compactSeed, loadedSeed, "Loaded seed is not the same as the initial seed.");
+                callback();
+            });
+        });
+    });
+});
