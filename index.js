b119f35ccbd8805fe0d0a96712bb64e5348d8b77
skutner <sava.dumitru.daniel@gmail.com>
2020-01-09 10:43:52 +0200

    exported function createHTTPBrickTransportStrategy from edfs

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-require("brickTransportStrategies/brickTransportStrategiesRegistry");
+require("./brickTransportStrategies/brickTransportStrategiesRegistry");
 module.exports = {
     attach(brickTransportStrategyName) {
         const EDFS = require("./lib/EDFS");
         return new EDFS(brickTransportStrategyName);
+    },
+    createHTTPBrickTransportStrategy(initialConfig) {
+        const HTTPBrickTransportStrategy = require("./brickTransportStrategies/HTTPBrickTransportStrategy");
+        return new HTTPBrickTransportStrategy(initialConfig);
     }
 };
 
