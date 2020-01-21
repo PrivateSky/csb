01a88d779217eafcf53c0031c90d15375baee526
skutner <sava.dumitru.daniel@gmail.com>
2020-01-21 09:46:22 +0200

    Refactored aliases structure in EDFS

--- brickTransportStrategies/HTTPBrickTransportStrategy.js
+++ brickTransportStrategies/HTTPBrickTransportStrategy.js
@@ -4,15 +4,19 @@ function HTTPBrickTransportStrategy(initialConfig) {
     const url = initialConfig;
 
     this.send = (name, data, callback) => {
-        $$.remote.doHttpPost(url + "/EDFS/alias/" + name, data, callback);
+        $$.remote.doHttpPost(url + "/EDFS/" + name, data, callback);
     };
 
     this.get = (name, callback) => {
-        $$.remote.doHttpGet(url + "/EDFS/alias/" + name, callback);
+        $$.remote.doHttpGet(url + "/EDFS/" + name, callback);
     };
 
     this.getHashForAlias = (alias, callback) => {
+        $$.remote.doHttpGet(url + "/EDFS/getVersions/" + alias, (err, hashesList) => callback(err, JSON.parse(hashesList.toString())));
+    };
 
+    this.attachHashToAlias = (alias, name, callback) => {
+        $$.remote.doHttpPost(url + "/EDFS/attachHashToAlias/" + name, alias, callback);
     };
 
     this.getLocator = () => {
