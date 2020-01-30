b457197d56c64b17683c5ccf00feb42959218c36
asac <asac.cool@gmail.com>
2020-01-30 15:24:31 +0200

    new api to create and edfs instance based on seed

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ module.exports = {
         const EDFS = require("./lib/EDFS");
         return new EDFS(brickTransportStrategyName);
     },
-
+    attachFromSeed(compactSeed){
+        const SEED = require("bar").Seed;
+        const seed = new SEED(compactSeed);
+        const transportStrategy = new this.HTTPBrickTransportStrategy(seed.getEndpoint());
+        const transportStrategyAlias = "seedBasedStrategy";
+        $$.brickTransportStrategiesRegistry.add(transportStrategyAlias, transportStrategy);
+        return this.attach(transportStrategyAlias);
+    },
     HTTPBrickTransportStrategy: require("./brickTransportStrategies/HTTPBrickTransportStrategy"),
     constants: constants
 };
