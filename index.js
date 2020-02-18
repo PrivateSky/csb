7125395763aac19718f9bf9052b5a19f25965c81
asaccool <asac.cool@gmail.com>
2020-02-18 18:35:31 +0200

    attachToEndpoint api

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ module.exports = {
         const EDFS = require("./lib/EDFS");
         return new EDFS(brickTransportStrategyName);
     },
+    attachToEndpoint(endpoint){
+        //TODO:test endpoint against regex to determine transport strategy type
+        //for now http will be used
+        const transportStrategy = new this.HTTPBrickTransportStrategy(endpoint);
+        const transportStrategyAlias = "seedBasedStrategy";
+        $$.brickTransportStrategiesRegistry.add(transportStrategyAlias, transportStrategy);
+        return this.attach(transportStrategyAlias);
+    },
     attachFromSeed(compactSeed){
         const SEED = require("bar").Seed;
         const seed = new SEED(compactSeed);
